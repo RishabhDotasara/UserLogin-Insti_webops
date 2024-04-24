220dfd39216026fd97c1ad004204547790f45ce3
Rishabh Dotasara <rishabhdotasara@gmail.com>
2024-04-24 15:53:34 +0530

    completed!

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,37 +1,66 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
-import CryptoJS from 'crypto-js';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import CryptoJS from "crypto-js";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
-
-  const [username,setUsername] = useState("");
-  const [password,setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
   const navigate = useNavigate();
-  const secretKey = "webops";
-  const loginUser = async ()=>{
-    const storedData = await localStorage.getItem('data');
-    const userData = await JSON.parse(storedData);
-    const decryptedPassword = await CryptoJS.AES.decrypt(userData.password,secretKey).toString(CryptoJS.enc.Utf8)
 
-    if (decryptedPassword == password)
-    {
-      navigate("/success")
+  const loginUser = async () => {
+    const storedData = localStorage.getItem("data");
+    const userData = JSON.parse(storedData);
+    const secretKey = "webops";
+  
+    console.log("Username:", username);
+    console.log("Password:", password);
+    console.log("UserData:", userData);
+  
+    if (userData) {
+      for (const user of userData) {
+        const decryptedPassword = await CryptoJS.AES.decrypt(
+          user.password,
+          secretKey
+        ).toString(CryptoJS.enc.Utf8);
+        console.log("Decrypted Password:", decryptedPassword);
+        console.log("Password:", password);
+        if (user.username === username && decryptedPassword === password) {
+          navigate("/success");
+          return; // Exit the loop and function once user is found
+        }
+      }
+      alert("Invalid username or password");
+    } else {
+      alert("Users not found. Please register a user!");
+      navigate("/signup");
     }
-    console.log(userData);
-  }
+  };
+  
 
   return (
-    <div className='login-page'>
+    <div className="login-page">
       <h2>Login</h2>
       <div className="inputs">
-        <input type="text" placeholder='Username'  onClick={(e)=>{setUsername(e.target.value)}}/>
-        <input type="password" placeholder='Password'  onClick={(e)=>{setPassword(e.target.value)}} />
-        <button className='btn' onClick={()=>{
-          loginUser();
-        }}>Login</button>
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button className="btn" onClick={loginUser}>
+          Login
+        </button>
       </div>
-      <Link to="/signup" style={{textDecoration:'none'}}>Signup?</Link>
+      <Link to="/signup" style={{ textDecoration: "none" }}>
+        Signup?
+      </Link>
     </div>
-  )
+  );
 }
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,37 +1,52 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import CryptoJS from 'crypto-js';
 import { useNavigate } from 'react-router-dom';
 
 export default function Signup() {
-
-  const [username,setUsername] = useState("");
-  const [password,setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [users, setUsers] = useState([]);
   const navigate = useNavigate();
-  const registerUser = async ()=>{
+
+  const registerUser = async () => {
+    const secretKey = "webops";
+    const encryptedPassword = CryptoJS.AES.encrypt(password, secretKey).toString();
+
+
+    const storedData = localStorage.getItem('data');
+    const usersData = JSON.parse(storedData) || [];
+
+    const search = usersData.filter((e)=>{if(e.username===username){return (e)}else{return(null)}})
+    if(search.length > 0)
+    {
+      alert("Username is taken!")
+    }
+    else {
+
+        const userToAdd = { username: username, password: encryptedPassword };
+        const updatedUsers = [...usersData, userToAdd];
     
-      const secretKey = "webops";
-      const encryptedPassword =   await CryptoJS.AES.encrypt(password, secretKey).toString()
-      const data = {username:username,password:encryptedPassword}
-      localStorage.setItem('data',JSON.stringify(data));
-      navigate('/');
+        setUsers(updatedUsers);
+        localStorage.setItem('data', JSON.stringify(updatedUsers));
+    
+        navigate('/');
+    }
   }
+
+  useEffect(() => {
+    console.log(users);
+  }, [users]); // Log users whenever it changes
 
   return (
     <div className='login-page'>
       <h2>SignUp</h2>
       <div className="inputs">
-        <input type="text" placeholder='Username' value={username} onChange={(e)=>{
-          setUsername(e.target.value);
-        }}/>
-        <input type="password" placeholder='Password' value={password} onChange={(e)=>{
-          setPassword(e.target.value);
-        }}/>
-        <button className='btn' onClick={()=>{
-          registerUser();
-        }}>Signup</button>
+        <input type="text" placeholder='Username' value={username} onChange={(e) => setUsername(e.target.value)} />
+        <input type="password" placeholder='Password' value={password} onChange={(e) => setPassword(e.target.value)} />
+        <button className='btn' onClick={registerUser}>Signup</button>
       </div>
-      <Link to="/" style={{textDecoration:'none'}}>Login?</Link>
+      <Link to="/" style={{ textDecoration: 'none' }}>Login?</Link>
     </div>
   )
 }
